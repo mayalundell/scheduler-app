0542dc128a844e9a29d636a8afa6b5fe3e0d58d1
mayalundell <75233949+mayalundell@users.noreply.github.com>
2022-01-06 12:17:41 -0600

    jk now it's all modularization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { hasConflict, getCourseTerm, getCourseNumber, terms, addScheduleTimes } from './utilities/times.js'
+import { CourseList } from './components/CourseList.js'
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -28,77 +29,9 @@ const schedule = {
   }
 };
 
-
-
-const toggle = (x, lst) => (
-  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
-); // selecting and unselecting courses
-
-
-
-const Course = ({ course, selected, setSelected }) => {
-  const isSelected = selected.includes(course);
-  const isDisabled = !isSelected && hasConflict(course, selected);
-  const style = {
-    backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
-  };
-  return (
-    <div className="card m-1 p-2" 
-      style={style}
-      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
-      <div className="card-body">
-        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-        <div className="card-text">{ course.title }</div>
-      </div>
-    </div>
-  );
-};
-
-
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
-
-const TermButton = ({term, setTerm, checked}) => (
-  <>
-    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
-      onChange={() => setTerm(term)} />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
-    { term }
-    </label>
-  </>
-);
-
-const TermSelector = ({term, setTerm}) => (
-  <div className="btn-group">
-  { 
-    Object.values(terms).map(value => (
-      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
-    ))
-  }
-  </div>
-);
-
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Fall');
-  const [selected, setSelected] = useState([]);
-  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-  
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-      { 
-        termCourses.map(course =>
-          <Course key={ course.id } course={ course }
-            selected={selected} setSelected={ setSelected } 
-          />) 
-      }
-      </div>
-    </>
-  );
-};
-
 
 const App = () => {
   const [schedule, setSchedule] = useState();
